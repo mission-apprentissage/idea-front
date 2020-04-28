0da9b59e4a464bb79ee1c6f8b2e6e58e9d507e19
Alan Le Ruyet <alan.leruyet@free.fr>
2020-04-28 10:37:50 +0200

    Wording

--- src/pages/DiplomaSelection/DiplomaSelection.js
+++ src/pages/DiplomaSelection/DiplomaSelection.js
@@ -21,7 +21,7 @@ const DiplomaSelection = () => {
       <Container>
         <Row>
           <Col xs="12">
-            <h2>Quel est le diplôme en lien avec le métier que tu as déjà ?</h2>
+            <h2>Sélectionne le diplôme que tu as déjà ?</h2>
           </Col>
         </Row>
         <Row>
