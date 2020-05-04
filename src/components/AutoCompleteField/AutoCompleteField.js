12e36c73bc56b59570416a3d696234776811932d
Alan Le Ruyet <alan.leruyet@free.fr>
2020-05-04 12:05:21 +0200

    Autocomplete WIP

--- src/components/AutoCompleteField/AutoCompleteField.js
+++ src/components/AutoCompleteField/AutoCompleteField.js
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
 import { useField, useFormikContext } from "formik";
-import { useCombobox } from 'downshift'
+import { useCombobox } from "downshift";
+import axios from "axios";
 
-const items = [ {value:"a",label:"Bleu"},
+/*const items = [ {value:"a",label:"Bleu"},
 {value:"b",label:"rouge"},
-{value:"c",label:"Vert"} ];
+{value:"c",label:"Vert"} ];*/
 
-const menuStyles = {
-  maxHeight: "180px",
-  overflowY: "auto",
-  width: "135px",
-  margin: 0,
-  borderTop: 0,
-  background: "white",
-  position: "absolute",
-  zIndex: 1000,
-  listStyle: "none",
-  padding: 0,
-  left: "135px"
-};
+//const items = [];
 
-const itemToString = (item) => { return item ? item.label : ""; };
-
-export const AutoCompleteField = ({ ...props }) => {
+export const AutoCompleteField = ({ itemToStringFunction, onInputValueChangeFunction, items, ...props }) => {
   const { setFieldValue } = useFormikContext();
   const [field] = useField(props);
-  
-  const [inputItems, setInputItems] = useState( items );
+
+  const [inputItems, setInputItems] = useState(items);
+
+  const itemToString = (item) => {
+    return item ? item.label : "";
+  };
+
+  const fetchAddresses = (value) => {
+    const addressURL = `https://api-adresse.data.gouv.fr/search/?limit=15&q=${value}`;
+    console.log(addressURL);
+    axios.get(addressURL).then((response) => {
+      //this.setState({ movies: response.data.results })
+      console.log(response.data.features);
+      const returnedItems = response.data.features.map((feature) => {
+        console.log(feature);
+        return { value: feature.geometry, label: feature.properties.label };
+      });
+
+      console.log("returned items : ", returnedItems);
+      setInputItems(returnedItems);
+    });
+  };
+
   const {
     isOpen,
     getToggleButtonProps,
@@ -40,40 +48,38 @@ export const AutoCompleteField = ({ ...props }) => {
     items: inputItems,
     itemToString,
     onInputValueChange: ({ inputValue }) => {
-      setInputItems(
+      fetchAddresses(inputValue);
+
+      /*setInputItems(
         items.filter(item =>
           item.label.toLowerCase().startsWith(inputValue.toLowerCase()),
         ),
-      )
+      )*/
     },
-  })
+  });
   return (
     <div>
       <label {...getLabelProps()}>Choose an element:</label>
-      <div  {...getComboboxProps()}>
+      <div {...getComboboxProps()}>
         <input {...getInputProps()} />
         <button {...getToggleButtonProps()} aria-label="toggle menu">
           &#8595;
         </button>
       </div>
-      <ul {...getMenuProps()} style={menuStyles} >
+      <ul {...getMenuProps()} className="autoCompleteMenu">
         {isOpen &&
           inputItems.map((item, index) => (
             <li
-              style={
-                highlightedIndex === index
-                  ? { backgroundColor: '#bde4ff' }
-                  : {}
-              }
-              key={`${item.value}${index}`}
-              {...getItemProps({ item:item.label, index })}
+              className={highlightedIndex === index ? "highlightedMenuItem" : ""}
+              key={`${index}`}
+              {...getItemProps({ item: item.label, index })}
             >
               {item.label}
             </li>
           ))}
       </ul>
     </div>
-  )
+  );
 };
 
 export default AutoCompleteField;
