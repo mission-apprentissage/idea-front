6d983606a8573401e90ee31a2963296054571d74
Alan Le Ruyet <alan.leruyet@free.fr>
2020-05-16 00:37:24 +0200

    Récupération des formations
    2 choix de métier possibles

--- src/pages/ApiTester/ApiTester.js
+++ src/pages/ApiTester/ApiTester.js
@@ -3,34 +3,65 @@ import axios from "axios";
 import { IdeaHeader } from "../../components";
 import { Button, Container, Row, Col, FormGroup, Label, Input } from "reactstrap";
 import "./apitester.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Formik, Form, ErrorMessage } from "formik";
+import { AutoCompleteField } from "../../components";
+import Training from "./Training";
 
+const baseUrl =
+  window.location.hostname === "localhost" ? "http://localhost:3000" : "https://idea-mna-api.herokuapp.com";
 
-const baseUrl = window.location.hostname==="localhost"?"http://localhost:3000":"https://idea-mna-api.herokuapp.com";
-
-const formationApi = baseUrl+"/formation";
-const romesApi = baseUrl+"/romes";
-const romeLabelsApi = baseUrl+"/romelabels";
+const formationApi = baseUrl + "/formation";
+const romesApi = baseUrl + "/romes";
+const romeLabelsApi = baseUrl + "/romelabels";
 
 const ApiTester = () => {
-  const [tD, setTD] = useState(null);
-  const [hasError, setHasError] = useState(null);
+  const jobItems = [
+    { label: "Maçon", value: "F1703" },
+    { label: "Boucher", value: "D1101" },
+    //{ label: "Opticien", value: "C0000" },
+  ];
 
-  const handleChange = (response) => {
-    setTD(response);
-    setHasError(null);
+  // indique l'attribut de l'objet contenant le texte de l'item sélectionné à afficher
+  const autoCompleteToStringFunction = (item) => {
+    return item ? item.label : "";
   };
 
-  const fetchTrainings = async () =>
-  {
-      const trainings = await fetch()
-  }
+  // Permet de sélectionner un élément dans la liste d'items correspondant à un texte entré au clavier
+  const compareAutoCompleteValues = (items, value) => {
+    return items.findIndex((element) => element.label.toLowerCase() === value.toLowerCase());
+  };
 
-  const handleSubmit = async () => {
-    
-    const response = await axios.get(formationApi, { params: { romes : "D1101" } });
+  // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
+  const updateValuesFromAutoComplete = (item, setFieldValue) => {
+    //setTimeout perme d'éviter un conflit de setState
+    setTimeout(() => {
+      setFieldValue("job", item);
+    }, 0);
+  };
+
+  const [trainings, setTrainings] = useState(null);
+
+  const handleSearchTrainingSubmit = async (values, { setSubmitting }) => {
+    const response = await axios.get(formationApi, { params: { romes: values.job.value } });
 
     console.log(response);
+    setTrainings(response.data);
+  };
 
+  const getResult = () => {
+    if (trainings) {
+      return (
+        <div>
+          {trainings.map((training, idx) => {
+            return <Training key={idx} training={training} />;
+          })}
+        </div>
+      );
+    } else {
+      return "";
+    }
   };
 
   return (
@@ -39,23 +70,52 @@ const ApiTester = () => {
       <Container>
         <Row>
           <Col xs="12">
-            <h2>Formulaire de test des APIs IDEA?</h2>
+            <h2>Test des APIs IDEA</h2>
           </Col>
         </Row>
         <Row>
           <div xs="12" sm="6" md="3">
             <Container>
               <Row>
-                <div>Filtre A</div>
+                <div>RECHERCHE DE FORMATIONS</div>
+                <Formik
+                  validate={(values) => {
+                    const errors = {};
+                    if (!values.job || !values.job.label || !values.job.value) {
+                      errors.job = "Sélectionne un métier";
+                    }
+                    return errors;
+                  }}
+                  initialValues={{ job: {} }}
+                  onSubmit={handleSearchTrainingSubmit}
+                >
+                  {({ isSubmitting }) => (
+                    <Form>
+                      <div className="formGroup">
+                        <FontAwesomeIcon icon={faSearch} />
+                        <AutoCompleteField
+                          items={jobItems}
+                          initialIsOpen={true}
+                          itemToStringFunction={autoCompleteToStringFunction}
+                          onSelectedItemChangeFunction={updateValuesFromAutoComplete}
+                          compareItemFunction={compareAutoCompleteValues}
+                          name="jobField"
+                          placeholder="ex: boucher"
+                        />
+                      </div>
+                      <ErrorMessage name="job" className="errorField" component="div" />
 
-                <Button type="submit" onClick={handleSubmit}>
-                  Valider
-                </Button>
+                      <Button type="submit" disabled={isSubmitting}>
+                        Valider
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
               </Row>
             </Container>
           </div>
-          <div xs="12" sm="6" md="3">
-            Résultat
+          <div xs="12" sm="6" md="9">
+            {getResult()}
           </div>
         </Row>
       </Container>
---
+++ src/pages/ApiTester/Training.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Training = ({ training }) => {
+  
+  
+  return (
+    <div className="resultCard">
+        {JSON.stringify(training)}
+          </div>
+  );
+};
+
+export default Training;
