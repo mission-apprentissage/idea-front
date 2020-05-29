e79dee27a4b8dc98e526c2d0324469e957c597be
Alan Le Ruyet <alan.leruyet@free.fr>
2020-05-29 13:28:25 +0200

    Finalisation présentation des listes pour démo

---
+++ src/pages/SearchDemo/LbbCompany.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LbbCompany = ({ company }) => {
+  console.log("lbb company : ", company);
+
+  return (
+    <div className="resultCard">
+      <div className="title">{company.name}</div>
+      <div className="body">        
+        Lieu : {company.city} - Distance : {company.distance} km(s)
+      </div>
+    </div>
+  );
+};
+
+export default LbbCompany;
