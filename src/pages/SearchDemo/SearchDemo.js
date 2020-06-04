149d825e70e2eaa595405db7a9af0224dc0a1fb0
Alan Le Ruyet <alan.leruyet@free.fr>
2020-06-04 10:13:46 +0200

    Externalisation formulaire

--- src/pages/SearchDemo/SearchDemo.js
+++ src/pages/SearchDemo/SearchDemo.js
@@ -2,60 +2,20 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Button, Row, Col } from "reactstrap";
 import "./searchdemo.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { Formik, Form, ErrorMessage } from "formik";
-import { AutoCompleteField } from "../../components";
-import { fetchAddresses } from "../../services/baseAdresse";
 import mapboxgl from "mapbox-gl";
 import Training from "./Training";
 import PeJob from "./PeJob";
 import LbbCompany from "./LbbCompany";
-
-const baseUrl =
-  window.location.hostname === "localhost" ? "http://localhost:3000" : "https://idea-mna-api.herokuapp.com";
+import baseUrl from "../../utils/baseUrl";
+import SearchForm from "./SearchForm";
 
 const formationsApi = baseUrl + "/formations";
 const jobsApi = baseUrl + "/jobs";
-const romeLabelsApi = baseUrl + "/romelabels";
 
 let currentMarkers = [];
 
-export const fetchRomes = async (value) => {
-  if (value) {
-    const response = await axios.get(romeLabelsApi, { params: { title: value } });
-
-    if (response.data instanceof Array) return response.data;
-    else return [];
-  } else return [];
-};
-
 const SearchDemo = () => {
-  // indique l'attribut de l'objet contenant le texte de l'item sélectionné à afficher
-  const autoCompleteToStringFunction = (item) => {
-    return item ? item.label : "";
-  };
 
-  // Permet de sélectionner un élément dans la liste d'items correspondant à un texte entré au clavier
-  const compareAutoCompleteValues = (items, value) => {
-    return items.findIndex((element) => element.label.toLowerCase() === value.toLowerCase());
-  };
-
-  // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
-  const updateValuesFromJobAutoComplete = (item, setFieldValue) => {
-    //setTimeout perme d'éviter un conflit de setState
-    setTimeout(() => {
-      setFieldValue("job", item);
-    }, 0);
-  };
-
-  // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
-  const updateValuesFromPlaceAutoComplete = (item, setFieldValue) => {
-    //setTimeout perme d'éviter un conflit de setState
-    setTimeout(() => {
-      setFieldValue("location", item);
-    }, 0);
-  };
 
   const [trainings, setTrainings] = useState(null);
   const [jobs, setJobs] = useState(null);
@@ -65,7 +25,7 @@ const SearchDemo = () => {
   const [mapState, setMapState] = useState({
     lat: 48.85341,
     lon: 2.3488,
-    zoom: 10,
+    zoom: 8,
   });
 
   const mapContainer = useRef(null);
@@ -173,24 +133,28 @@ const SearchDemo = () => {
     // positionnement des marqueurs bonne boîte
     if (jobs && jobs.lbbCompanies && jobs.lbbCompanies.companies_count) {
       jobs.lbbCompanies.companies.map((company, idx) => {
-        currentMarkers.push(new mapboxgl.Marker({ color: "red" })
-          .setLngLat([company.lon, company.lat])
-          .setPopup(new mapboxgl.Popup().setHTML(`${company.name}<br />${company.address}`))
-          .addTo(map));
+        currentMarkers.push(
+          new mapboxgl.Marker({ color: "red" })
+            .setLngLat([company.lon, company.lat])
+            .setPopup(new mapboxgl.Popup().setHTML(`${company.name}<br />${company.address}`))
+            .addTo(map)
+        );
       });
     }
 
     // positionnement des marqueurs PE
     if (jobs && jobs.peJobs && jobs.peJobs.length) {
       jobs.peJobs.map((job, idx) => {
-        currentMarkers.push(new mapboxgl.Marker({ color: "green" })
-          .setLngLat([job.lieuTravail.longitude, job.lieuTravail.latitude])
-          .setPopup(
-            new mapboxgl.Popup().setHTML(
-              `${job.intitule}<br />${job.entreprise ? job.entreprise.nom : ""}<br />${job.lieuTravail.libelle}`
+        currentMarkers.push(
+          new mapboxgl.Marker({ color: "green" })
+            .setLngLat([job.lieuTravail.longitude, job.lieuTravail.latitude])
+            .setPopup(
+              new mapboxgl.Popup().setHTML(
+                `${job.intitule}<br />${job.entreprise ? job.entreprise.nom : ""}<br />${job.lieuTravail.libelle}`
+              )
             )
-          )
-          .addTo(map));
+            .addTo(map)
+        );
       });
     }
   };
@@ -269,79 +233,26 @@ const SearchDemo = () => {
   };
 
   const getSearchForm = () => {
-    return (
-      <>
-        <h1>Test des APIs IDEA</h1>
-        <Formik
-          validate={(values) => {
-            const errors = {};
-            if (!values.job || !values.job.label || !values.job.rome) {
-              errors.job = "Sélectionne un métier";
-            }
-            return errors;
-          }}
-          initialValues={{ job: {} }}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Row>
-                <Col xs="12">
-                  <div className="formGroup">
-                    <FontAwesomeIcon icon={faSearch} />
-                    <AutoCompleteField
-                      items={[]}
-                      itemToStringFunction={autoCompleteToStringFunction}
-                      onSelectedItemChangeFunction={updateValuesFromJobAutoComplete}
-                      compareItemFunction={compareAutoCompleteValues}
-                      onInputValueChangeFunction={fetchRomes}
-                      name="jobField"
-                      placeholder="ex: boucher"
-                    />
-                  </div>
-                  <ErrorMessage name="job" className="errorField" component="div" />
-                </Col>
-                <Col xs="12">
-                  <div className="formGroup">
-                    <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    <AutoCompleteField
-                      items={[]}
-                      itemToStringFunction={autoCompleteToStringFunction}
-                      onSelectedItemChangeFunction={updateValuesFromPlaceAutoComplete}
-                      compareItemFunction={compareAutoCompleteValues}
-                      onInputValueChangeFunction={fetchAddresses}
-                      name="placeField"
-                      placeholder="ex: Nantes"
-                    />
-                  </div>
-                  <ErrorMessage name="location" className="errorField" component="div" />
-                </Col>
-              </Row>
-
-              <Button type="submit" disabled={isSubmitting}>
-                Valider
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </>
-    );
+    return <SearchForm handleSubmit={handleSubmit} />
   };
 
   return (
     <div className="page demoPage">
       <Row>
-        <Col xs="12" lg="3">
+        <Col xs="12" lg="8">
+          <div className="mapContainer">
+            {getMap()}
+          </div>
+        </Col>
+        <Col xs="12" lg="4">
           <div className="leftCol">
+            
             lat : {mapState.lat} - long : {mapState.lon} - zoom : {mapState.zoom}
             <br />
             {getSearchForm()}
             {getTrainingResult()}
             {getJobResult()}
           </div>
-        </Col>
-        <Col xs="12" lg="9">
-          {getMap()}
         </Col>
       </Row>
     </div>
---
+++ src/pages/SearchDemo/SearchForm.js
@@ -0,0 +1,109 @@
+import React from "react";
+import axios from "axios";
+import { Button, Row, Col } from "reactstrap";
+import "./searchdemo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Formik, Form, ErrorMessage } from "formik";
+import { AutoCompleteField } from "../../components";
+import { fetchAddresses } from "../../services/baseAdresse";
+import baseUrl from "../../utils/baseUrl";
+
+const romeLabelsApi = baseUrl + "/romelabels";
+
+export const fetchRomes = async (value) => {
+  if (value) {
+    const response = await axios.get(romeLabelsApi, { params: { title: value } });
+
+    if (response.data instanceof Array) return response.data;
+    else return [];
+  } else return [];
+};
+
+const SearchForm = (props) => {
+  // indique l'attribut de l'objet contenant le texte de l'item sélectionné à afficher
+  const autoCompleteToStringFunction = (item) => {
+    return item ? item.label : "";
+  };
+
+  // Permet de sélectionner un élément dans la liste d'items correspondant à un texte entré au clavier
+  const compareAutoCompleteValues = (items, value) => {
+    return items.findIndex((element) => element.label.toLowerCase() === value.toLowerCase());
+  };
+
+  // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
+  const updateValuesFromJobAutoComplete = (item, setFieldValue) => {
+    //setTimeout perme d'éviter un conflit de setState
+    setTimeout(() => {
+      setFieldValue("job", item);
+    }, 0);
+  };
+
+  // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
+  const updateValuesFromPlaceAutoComplete = (item, setFieldValue) => {
+    //setTimeout perme d'éviter un conflit de setState
+    setTimeout(() => {
+      setFieldValue("location", item);
+    }, 0);
+  };
+
+  return (
+    <>
+      <h1>Test des APIs IDEA</h1>
+      <Formik
+        validate={(values) => {
+          const errors = {};
+          if (!values.job || !values.job.label || !values.job.rome) {
+            errors.job = "Sélectionne un métier";
+          }
+          return errors;
+        }}
+        initialValues={{ job: {} }}
+        onSubmit={props.handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Row>
+              <Col xs="12">
+                <div className="formGroup">
+                  <FontAwesomeIcon icon={faSearch} />
+                  <AutoCompleteField
+                    items={[]}
+                    itemToStringFunction={autoCompleteToStringFunction}
+                    onSelectedItemChangeFunction={updateValuesFromJobAutoComplete}
+                    compareItemFunction={compareAutoCompleteValues}
+                    onInputValueChangeFunction={fetchRomes}
+                    name="jobField"
+                    placeholder="ex: boucher"
+                  />
+                </div>
+                <ErrorMessage name="job" className="errorField" component="div" />
+              </Col>
+              <Col xs="12">
+                <div className="formGroup">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} />
+                  <AutoCompleteField
+                    items={[]}
+                    itemToStringFunction={autoCompleteToStringFunction}
+                    onSelectedItemChangeFunction={updateValuesFromPlaceAutoComplete}
+                    compareItemFunction={compareAutoCompleteValues}
+                    onInputValueChangeFunction={fetchAddresses}
+                    name="placeField"
+                    placeholder="ex: Nantes"
+                  />
+                </div>
+                <ErrorMessage name="location" className="errorField" component="div" />
+              </Col>
+            </Row>
+
+            <Button type="submit" disabled={isSubmitting}>
+              Valider
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default SearchForm;
