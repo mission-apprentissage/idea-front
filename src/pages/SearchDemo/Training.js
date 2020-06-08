850196f5936eca745e78c08445ee1a7fca18d2cb
Alan Le Ruyet <alan.leruyet@free.fr>
2020-06-08 18:20:17 +0200

    Chartage cartes formations

--- src/pages/SearchDemo/Training.js
+++ src/pages/SearchDemo/Training.js
@@ -8,21 +8,23 @@ const Training = ({ training }) => {
         <img className="cardIcon" src={trainingIcon} />
         <span className="cardDistance">{Math.round(training.sort[0])} km(s) du lieu de recherche</span>
       </div>
-      <div className="title">{training.source.intitule_long}</div>
+      <div className="title">{training.source.rncp_intitule}</div>
       <div className="body">
+        {training.source.entreprise_raison_sociale}
+        <div className="companyAddress">{training.source.adresse}</div>
         {training.source.onisep_url ? (
-          <a href={training.source.onisep_url} target="_blank" rel="noopener noreferrer">
-            Lien ONISEP
-          </a>
+          <div>
+            <a href={training.source.onisep_url} target="_blank" rel="noopener noreferrer">
+              Lien ONISEP
+            </a>
+          </div>
         ) : (
           ""
         )}
-        <br />
-        Diplôme : {training.source.diplome}
-        <br />
-        Code postal : {training.source.code_postal} 
       </div>
-      <div className='knowMore'><a href="#">En savoir plus</a></div>
+      <div className="knowMore">
+        <a href="#">En savoir plus</a>
+      </div>
     </div>
   );
 };
