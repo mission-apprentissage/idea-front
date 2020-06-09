b1cc07369e3db96df711fb2cbbc69ae8c45834e7
Alan Le Ruyet <alan.leruyet@free.fr>
2020-06-09 17:47:06 +0200

    Zoom 10 après flyTo

--- src/pages/SearchTraining/SearchTraining.js
+++ src/pages/SearchTraining/SearchTraining.js
@@ -81,9 +81,8 @@ const SearchTraining = () => {
     clearMarkers();
     // centrage de la carte sur le lieu de recherche
     searchCenter = [values.location.value.coordinates[0], values.location.value.coordinates[1]];
-    const cZoom = map.getZoom();
-    map.flyTo({ center: searchCenter });
-    map.setZoom(cZoom);
+
+    map.flyTo({ center: searchCenter, zoom:10 });
 
     searchForTrainings(values);
     searchForJobs(values);
@@ -142,10 +141,9 @@ const SearchTraining = () => {
   };
 
   const setTrainingMarkers = (trainingList) => {
-    
     // centrage sur formation la plus proche
     const centerCoords = trainingList[0].coords.split(","); 
-    map.flyTo({ center: [centerCoords[1],centerCoords[0]] });
+    map.flyTo({ center: [centerCoords[1], centerCoords[0]], zoom: 10 });
 
     trainingList.map((training, idx) => {
       const coords = training.coords.split(",");
