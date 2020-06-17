f8dca01ae33bd6ebf1c6c647bed93240b1918032
Alan Le Ruyet <alan.leruyet@free.fr>
2020-06-17 16:51:08 +0200

    Amélioration des retours de la BAN

--- src/services/baseAdresse.js
+++ src/services/baseAdresse.js
@@ -2,12 +2,30 @@ import axios from "axios";
 
 export const fetchAddresses = (value) => {
   if (value) {
-    const limit = 7;
-    const addressURL = `https://api-adresse.data.gouv.fr/search/?limit=${limit}&q=${value}`;
+    const limit = 10;
+    let addressURL = `https://api-adresse.data.gouv.fr/search/?limit=${limit}&q=${value}`;
+
+    if (value.length < 6 && isNaN(value[0])) addressURL += "&type=municipality";
 
     return axios.get(addressURL).then((response) => {
+      response.data.features.sort((a, b) => { // tri des résultats avec mise en avant des villes de plus grande taille en premier
+        if (a.properties.population && b.properties.population)
+          return b.properties.population - a.properties.population;
+        else if (a.properties.population) return -1;
+        else if (b.properties.population) return 1;
+        else return 0;
+      });
+
       const returnedItems = response.data.features.map((feature) => {
-        return { value: feature.geometry, insee: feature.properties.citycode, zipcode: feature.properties.postcode, label: feature.properties.label };
+        let label = feature.properties.label;
+        if (label.indexOf(feature.properties.postcode) < 0) label += " " + feature.properties.postcode; // ajout du postcode dans le label pour les villes
+
+        return {
+          value: feature.geometry,
+          insee: feature.properties.citycode,
+          zipcode: feature.properties.postcode,
+          label,
+        };
       });
 
       //console.log("returned items : ", returnedItems);
