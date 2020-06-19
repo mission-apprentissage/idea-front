8777c8ecf38fbd4f8531901062e90cad3ca110d9
Alan Le Ruyet <alan.leruyet@free.fr>
2020-06-19 16:22:01 +0200

    Externalisation composant RadioButton

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import ScrollToTop from "./ScrollToTop";
 import DatePickerField from "./DatePickerField";
 import AutoCompleteField from "./AutoCompleteField";
 import LogoIdea from "./LogoIdea";
+import RadioButton from "./RadioButton";
 
-export { AutoCompleteField, DatePickerField, JobAdvice, IdeaHeader, IdeaTitle, Footer, ScrollToTop, LogoIdea };
+export { RadioButton, AutoCompleteField, DatePickerField, JobAdvice, IdeaHeader, IdeaTitle, Footer, ScrollToTop, LogoIdea };
