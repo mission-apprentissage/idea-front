7d03bf675645568c14dd64e1798148b6d4e77d07
Alan Le Ruyet <alan.leruyet@free.fr>
2020-07-20 14:31:47 +0200

    Ajout de l'accès à l'api catalogue dans la config

--- src/config.js
+++ src/config.js
@@ -2,14 +2,35 @@ const config = {
   local: {
     urls: [/^"localhost"$/g],
     baseUrl: "http://localhost:3000",
+    aws: {
+      apiGateway: {
+        name: "api",
+        region: "local",
+        endpoint: "http://localhost:3001/local",
+      },
+    },
   },
   dev: {
     urls: [/^develop--idea-mna.netlify.app$/g /*, /^deploy-preview-[0-9]+--idea-front-dev.netlify.com$/g*/],
     baseUrl: "https://idea-mna-api-dev.herokuapp.com",
+    aws: {
+      apiGateway: {
+        name: "api",
+        region: "eu-west-3",
+        endpoint: "https://r7mayzn08d.execute-api.eu-west-3.amazonaws.com/dev",
+      },
+    },
   },
   prod: {
     urls: [/^idea-mna.netlify.app$/g],
     baseUrl: "https://idea-mna-api.herokuapp.com",
+    aws: {
+      apiGateway: {
+        name: "api",
+        region: "eu-west-3",
+        endpoint: "https://c7a5ujgw35.execute-api.eu-west-3.amazonaws.com/prod",
+      },
+    },
   },
 };
 
@@ -28,11 +49,11 @@ export const getEnvName = () => {
 export const getConfig = (envName) => {
   switch (envName) {
     case "prod":
-      return config.prod.baseUrl;
+      return config.prod;
     case "dev":
-      return config.dev.baseUrl;
+      return config.dev;
     default:
-      return config.local.baseUrl;
+      return config.local;
   }
 };
 
