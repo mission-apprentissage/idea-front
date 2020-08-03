e3d4d9e78662b1c11e85674f7b482bd24d9cb8db
Alan Le Ruyet <alan.leruyet@free.fr>
2020-08-03 23:46:29 +0200

    Composant avec le texte et style pour annoncer 0 opportunité de stage

---
+++ src/pages/SearchForTrainingsAndJobs/components/NoJobResult.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoJobResult = () => {
+  return <div className="bold jobColor noOpportunityFound">Aucune opportunité de stage trouvée</div>;
+};
+
+export default NoJobResult;
