557f42113cdb53ce2a4c00ddcce8fda5e1fbb763
Alan Le Ruyet <alan.leruyet@free.fr>
2020-08-04 00:15:38 +0200

    Masquage texte pas d'offre sur version formations seulement

--- src/pages/SearchForTrainingsAndJobs/components/NoJobResult.js
+++ src/pages/SearchForTrainingsAndJobs/components/NoJobResult.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const NoJobResult = () => {
-  return <div className="bold jobColor noOpportunityFound">Aucune opportunité de stage trouvée</div>;
+const NoJobResult = ({ isTrainingOnly }) => {
+  return isTrainingOnly ? (
+    ""
+  ) : (
+    <div className="bold jobColor noOpportunityFound">Aucune opportunité de stage trouvée</div>
+  );
 };
 
 export default NoJobResult;
