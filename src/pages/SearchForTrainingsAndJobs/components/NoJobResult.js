f33e70d2ea804eef60d66426e2b1a6fed697b8a5
Alan Le Ruyet <alan.leruyet@free.fr>
2020-08-07 11:37:05 +0200

    Wording

--- src/pages/SearchForTrainingsAndJobs/components/NoJobResult.js
+++ src/pages/SearchForTrainingsAndJobs/components/NoJobResult.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const NoJobResult = ({ isTrainingOnly }) => {
-  return isTrainingOnly ? (
-    ""
-  ) : (
-    <div className="bold jobColor noOpportunityFound">Aucune opportunité de stage trouvée</div>
-  );
+  return isTrainingOnly ? "" : <div className="bold jobColor noOpportunityFound">Aucune entreprise trouvée</div>;
 };
 
 export default NoJobResult;
