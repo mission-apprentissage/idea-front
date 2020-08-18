65e1e5152ac678f583a77f7429af03b6232a2ad7
Alan Le Ruyet <alan.leruyet@free.fr>
2020-08-18 17:24:32 +0200

    adaptation avec des paramètres personnalisés

--- src/services/googleAnalytics.js
+++ src/services/googleAnalytics.js
@@ -1,12 +1,16 @@
-export const gtag = (category, action, label) => {
-  let params = {};
+export const gtag = (category, action, label, params) => {
+  try {
+    let p = params || {};
 
-  if (category) params.event_category = category;
+    if (category) p.event_category = category;
 
-  if (label) params.event_label = label;
+    if (label) p.event_label = label;
 
-  if (window.gtag) {
-    console.log("gtag : ", category, action, label);
-    window.gtag("event", action, params);
+    if (window.gtag) {
+      console.log("gtag : ", category, action, p);
+      window.gtag("event", action, p);
+    }
+  } catch (err) {
+    console.log("Error logging to google analytics : ", err);
   }
 };
