a00ee04fc363bf56a9b4fe98a06d67d3fa9ecd6e
Alan Le Ruyet <alan.leruyet@free.fr>
2020-08-19 15:50:15 +0200

    Envoi d'un message au frame parent pour le retour à la home LBA

--- src/components/LogoIdea/LogoIdea.js
+++ src/components/LogoIdea/LogoIdea.js
@@ -5,10 +5,18 @@ import "./logoidea.css";
 import { Row, Col } from "reactstrap";
 
 const LogoIdea = () => {
+  const goToLbaHome = () => {
+    let p = { type: "goToPage", page: "/" };
+    window.parent.postMessage(p, "*");
+    //console.log("sentMessageToParent : ",p);
+  };
+
   return (
     <Row className="logoIdea">
       <Col xs="4">
-        <img src={logoLBA} alt="La Bonne Alternance" />
+        <a href="#" onClick={goToLbaHome}>
+          <img src={logoLBA} alt="La Bonne Alternance" />
+        </a>
       </Col>
       <Col xs="8">
         <h1>Trouvez votre apprentissage</h1>
