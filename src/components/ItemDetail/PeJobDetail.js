ad9039d1d6a88098dbf5134a76737f8f0813ac18
Alan Le Ruyet <alan.leruyet@free.fr>
2020-09-02 14:52:19 +0200

    Décoration lien externe de l'offre PE

--- src/components/ItemDetail/PeJobDetail.js
+++ src/components/ItemDetail/PeJobDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import moment from "moment";
 import { gtag } from "../../services/googleAnalytics";
 import infoIcon from "../../assets/icons/info.svg";
+import linkIcon from "../../assets/icons/link.svg";
 
 const PeJobDetail = ({ job }) => {
   //console.log("peJob : ", job);
@@ -75,14 +76,17 @@ const PeJobDetail = ({ job }) => {
           ""
         )}
         <div className="sectionTitle">Retrouvez l'offre sur Pôle emploi</div>
-        <div className="description">
+        <div>
           <a
             target="poleemploi"
             href={`https://candidat.pole-emploi.fr/offres/recherche/detail/${job.id}`}
             onClick={() => {
               logClickedLink("Offre Pôle emploi");
             }}
-          >{`https://candidat.pole-emploi.fr/offres/recherche/detail/${job.id}`}</a>
+          >
+            <img className="linkIcon" src={linkIcon} alt="" />{" "}
+            {`https://candidat.pole-emploi.fr/offres/recherche/detail/${job.id}`}
+          </a>
         </div>
         <div className="blueAdvice">
           <div className="floatLeft">
