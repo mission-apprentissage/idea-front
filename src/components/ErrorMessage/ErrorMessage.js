72e01e8d8912267ed1af006bb0ae8d91f55ad72f
Alan Le Ruyet <alan.leruyet@free.fr>
2020-09-10 22:56:45 +0200

    Format message d'erreur spécifique pour la colonne de droite

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./errorMessage.css";
 import errorAlertIcon from "../../assets/icons/errorAlert.svg";
+import searchingPeople from "../../assets/icons/searchingPeople.svg";
 
 const ErrorMessage = ({ type, message }) => {
-
-    console.log("message : ",message);
+  console.log("message : ", message);
 
   return (
-    <div className="errorMessage">
-      <img src={errorAlertIcon} alt="" />
-      {message}
-    </div>
+    <>
+      {type === "column" ? <img src={searchingPeople} alt="" /> : ""}
+      <div className="errorMessage">
+        <img src={errorAlertIcon} alt="" />
+        {message}
+      </div>
+      {type === "column" ? (
+        <div className="dontPanic">
+          <h3>
+            Pas de panique <span className="orange">!</span>
+          </h3>
+          Il y a forcément un résultat qui vous attend, veuillez revenir ultérieurement
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
