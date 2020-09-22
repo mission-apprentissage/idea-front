9800a040a646ce55a511e61f476c54703e3f9b95
Alan Le Ruyet <alan.leruyet@free.fr>
2020-09-22 17:52:31 +0200

    Chartage page 404

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import peopleIcon from "../../assets/icons/searchingPeople.svg";
+import errorAlertIcon from "../../assets/icons/errorAlert.svg";
 import "./notFound.css";
 
 const NotFound = () => (
   <div className="page not-found">
-    <div id="oopss">
+    <div class="error">
+      <img src={peopleIcon} />
       <div id="error-text">
         <span>404</span>
-        <h1>PAGE INCONNUE</h1>
-        <p className="hmpg">
-          <Link to="/" className="back">
-            Retour à l'accueil
-          </Link>
-        </p>
+        <h1>PAGE INCONNUE !</h1>
+        <div className="errorMessage">
+          <img src={errorAlertIcon} alt="" />
+          Aucun contenu ne correspond à cette adresse
+        </div>
       </div>
+
+      <Link to="/" className="back">
+          Retour à l'accueil
+      </Link>
+
     </div>
   </div>
 );
