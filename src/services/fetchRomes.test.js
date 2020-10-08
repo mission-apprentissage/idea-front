0f562babd8a4c0f950059ba7d507dbbeca8f1fa1
David Boureau <bdavidxyz@gmail.com>
2020-10-08 15:50:18 +0200

    working auto test !!

---
+++ src/services/fetchRomes.test.js
@@ -0,0 +1,45 @@
+import fetchRomes from './fetchRomes';
+import _ from 'lodash';
+
+describe('fetchRomes', () => {
+
+    it('Should return an empty array if no value is given', async () => {
+      // given
+      const value = null
+      // when
+      const res = await fetchRomes(value)
+      // then
+      expect(res).toEqual([]);
+    });
+
+    it('Should return response.data.labelsAndRomes if remote API replied correctly', async () => {
+      // given
+      const value = 'plomberie'
+      const urlMock = 'urlMock'
+      const remoteResponse = {data: {labelsAndRomes: ['remotely_returned_array']}}
+      const mockedRemoteCall = jest.fn().mockReturnValue(remoteResponse)
+      const axiosMock = {get: mockedRemoteCall}
+      // when
+      const res = await fetchRomes(value, _.noop, urlMock, axiosMock)
+      // then
+      expect(res).toEqual(['remotely_returned_array']);
+      expect(mockedRemoteCall).toHaveBeenCalledWith('urlMock/romelabels', {params: { title: 'plomberie'}});
+    });
+
+    it('Should return empty array if response.data.error is filled', async () => {
+      // given
+      const value = 'plomberie'
+      const urlMock = 'urlMock'
+      const remoteResponse = {data: {error: 'remote_error_message'}}
+      const mockedRemoteCall = jest.fn().mockReturnValue(remoteResponse)
+      const mockedErrorFn = jest.fn()
+      const axiosMock = {get: mockedRemoteCall}
+      // when
+      const res = await fetchRomes(value, mockedErrorFn, urlMock, axiosMock)
+      // then
+      expect(res).toEqual(undefined);
+      expect(mockedRemoteCall).toHaveBeenCalledWith('urlMock/romelabels', {params: { title: 'plomberie'}});
+      expect(mockedErrorFn).toHaveBeenCalled();
+    });
+
+});
