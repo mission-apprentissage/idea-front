e4762c7d6b3feb4b19a051950cb96d54942f6606
David Boureau <bdavidxyz@gmail.com>
2020-10-08 10:40:43 +0200

    a first green test !!!

---
+++ src/services/simple.js
@@ -0,0 +1,4 @@
+
+export default function(value) {
+  return 2 + value;  
+};
---
+++ src/services/simple.test.js
@@ -0,0 +1,11 @@
+import simple from './simple';
+
+describe('simple', () => {
+
+    it('should return value, plus 2', () => {
+
+        expect(simple(40)).toEqual(42);
+
+    });
+
+});
