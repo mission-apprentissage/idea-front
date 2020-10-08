59bcbc3bb7602a50eb64a1c7d6e625757bab099a
David Boureau <bdavidxyz@gmail.com>
2020-10-08 10:22:50 +0200

    fetchRomes services externalized

---
+++ src/services/fetchRomes.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import baseUrl from "../utils/baseUrl";
+import get from 'lodash/get'
+
+export default async function (value, localBaseUrl=baseUrl, localAxios=axios) {
+  
+  const romeLabelsApi = localBaseUrl + "/romelabels";
+
+  if (value) {
+    const response = await localAxios.get(romeLabelsApi, { params: { title: value } });
+
+    if (response.data.labelsAndRomes) {
+      return response.data.labelsAndRomes;
+    } else {
+      if (response.data.error) {
+        console.log("error!!!!!!!!!!!!!!!!!!!!!!");
+        logError("Rome API error", `Rome API error ${response.data.error}`);
+      }
+      return [];
+    }
+  } else {
+    return [];
+  }
+};
