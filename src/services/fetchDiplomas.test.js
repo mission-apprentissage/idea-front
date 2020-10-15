463b892fe1e11c76e241c42276009721a6b2b66e
David Boureau <bdavidxyz@gmail.com>
2020-10-15 11:30:22 +0200

    all tests pass

--- src/services/fetchDiplomas.test.js
+++ src/services/fetchDiplomas.test.js
@@ -39,4 +39,32 @@ describe('fetchDiplomas', () => {
       expect(mockedLoggerFn).toHaveBeenCalledWith("Diploma API error", "Diploma API error remote_error_message");
       expect(res).toEqual([]);
     });
+
+    it('error case : axios do NOT returns expected data', async () => {
+      // given
+      const mockedErrorFn = jest.fn()
+      const mockedLoggerFn = jest.fn()
+      const axiosStub = {get: jest.fn().mockReturnValue({data: {unexpected_prop: 'unexpected_val'}})}
+      // when
+      const res = await fetchDiplomas(["D1208", "D1203"], mockedErrorFn, 'urlMock', axiosStub, {}, mockedLoggerFn)
+      // then
+      expect(mockedErrorFn).toHaveBeenCalled();
+      expect(mockedLoggerFn).toHaveBeenCalledWith("Diploma API error : API call worked, but returned unexpected data");
+      expect(res).toEqual([]);
+    });
+
+    it('error case : user simulated an error through URL query param', async () => {
+      // given
+      const mockedErrorFn = jest.fn()
+      const mockedLoggerFn = jest.fn()
+      const axiosStub = {get: jest.fn().mockReturnValue({data: ['remotely_returned_array']})}
+      // when
+      const res = await fetchDiplomas(["D1208", "D1203"], mockedErrorFn, 'urlMock', axiosStub, {location:{href:'anyurl.com?diplomaError=true'}}, mockedLoggerFn)
+      // then
+      expect(mockedErrorFn).toHaveBeenCalled();
+      expect(mockedLoggerFn).toHaveBeenCalledWith('Diploma API error simulated with a query param :)');
+      expect(res).toEqual([]);
+    });
+
+
 });
